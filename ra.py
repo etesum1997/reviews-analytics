683b56d70f031ae5c7eb77c715bbd0122a503a78
Summer Huang <etesum1997@gmail.com>
2020-10-16 01:58:01 +0800

    add filtering function

--- ra.py
+++ ra.py
@@ -1,7 +1,7 @@
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
-    for line in f:
+    for line in f: # 寫for loop來一筆一筆讀裡面的資料
         data.append(line)
         count += 1 # count = count + 1
         if count % 10000 == 0:
@@ -11,3 +11,17 @@ sum_len = 0 # 總長度
 for d in data:
     sum_len += len(d) # sum_len = sum_len + len(d) # 每筆長度加上前面累積的總長，再存回總總長
 print('留言的平均長度為', sum_len / len(data))
+
+new = []
+for d in data: # 把data裡的資料一筆一筆讀出來
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆資料小於100字')
+print(new[5])
+
+awful = []
+for d in data:
+    if 'awful' in d:
+        awful.append(d)
+print('一共有', len(awful), '筆資料提及\'awful\'')
+print('For example:', '\'', awful[0], '\'')
