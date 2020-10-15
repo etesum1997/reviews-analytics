4c7d6f087f91f51d9f0a6255cd19605ba16ae795
Summer Huang <etesum1997@gmail.com>
2020-10-15 20:59:40 +0800

    calc avg len

--- ra.py
+++ ra.py
@@ -4,9 +4,10 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1 # count = count + 1
-        if count % 1000 == 0:
+        if count % 10000 == 0:
             print(len(data)) # 若想知道讀取檔案的進度（讀到第幾筆），可以設定count，再把data長度印出來
-print(len(data))
-print(data[100])
-print('-')
-print(data[105])
+print('檔案讀取完了，總共有', len(data), '筆資料')
+sum_len = 0 # 總長度
+for d in data:
+    sum_len += len(d) # sum_len = sum_len + len(d) # 每筆長度加上前面累積的總長，再存回總總長
+print('留言的平均長度為', sum_len / len(data))
